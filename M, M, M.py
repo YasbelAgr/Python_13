0a25c2df4e1d1df4abd5564e6ce8cdf6337a3149
YasbelAgr <yasbelnac06@gmail.com>
2024-12-04 21:33:59 -0600

    first commit

---
+++ M, M, M.py
@@ -0,0 +1,10 @@
+
+from statistics import mean, median, mode
+
+numeros = [1, 2, 2, 3, 4, 4, 4, 5, 6]
+media = mean(numeros)
+mediana = median(numeros)
+moda = mode(numeros)
+print("media", media)
+print("mediana", mediana)
+print("moda", moda)
